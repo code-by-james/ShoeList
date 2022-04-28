d9c404af3a5f08292a76cb7327a8317e0919c5e4
James <james@marketingbyjames.co.uk>
2022-04-28 16:00:09 +0100

    Add project files.

---
+++ Shoe.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShoeList
+{
+    class Shoe
+    {
+        public Style Style { get; private set; }
+
+        public string Color { get; private set; }
+
+        public string Description
+        {
+            get
+            {
+                return $"A {Color} {Style}";
+            }
+        }
+
+        public Shoe(Style style, string color)
+        {
+            Style = style;
+            Color = color;
+        }
+    }
+}
